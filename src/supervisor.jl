c56cc497467f3be91d701699f3d2fbd633d8a4fb
Darryl Gamroth <dgamroth@rubus.ca>
2025-12-30 23:18:50 -0800

    Align supervisor_do_work! time fetch ordering

--- src/supervisor.jl
+++ src/supervisor.jl
@@ -328,11 +328,11 @@ function supervisor_do_work!(
     qos_assembler::Aeron.FragmentAssembler;
     fragment_limit::Int32 = DEFAULT_FRAGMENT_LIMIT,
 )
+    fetch!(state.clock)
+    now_ns = UInt64(Clocks.time_nanos(state.clock))
     work_count = 0
     work_count += poll_control!(state, control_assembler, fragment_limit)
     work_count += poll_qos!(state, qos_assembler, fragment_limit)
-    fetch!(state.clock)
-    now_ns = UInt64(Clocks.time_nanos(state.clock))
     work_count += poll_timers!(state, now_ns)
     return work_count
 end
