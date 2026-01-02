a75858629ae02c5a0bca50a0fd460e3b7ddf9f71
Darryl Gamroth <dgamroth@rubus.ca>
2026-01-02 11:43:06 -0800

    Route attach requests through driver lifecycle

--- src/driver/lifecycle_handlers.jl
+++ src/driver/lifecycle_handlers.jl
@@ -4,6 +4,27 @@
     return Hsm.EventHandled
 end
 
+@inline function reject_attach!(state::DriverState, msg::ShmAttachRequest.Decoder)
+    correlation_id = ShmAttachRequest.correlationId(msg)
+    emit_attach_response!(
+        state,
+        correlation_id,
+        DriverResponseCode.REJECTED,
+        "driver draining",
+        nothing,
+    )
+    return Hsm.EventHandled
+end
+
+@on_event function(sm::DriverLifecycle, ::Running, ::AttachRequest, state::DriverState)
+    handle_attach_request!(state, state.runtime.attach_decoder)
+    return Hsm.EventHandled
+end
+
+@on_event function(sm::DriverLifecycle, ::Draining, ::AttachRequest, state::DriverState)
+    return reject_attach!(state, state.runtime.attach_decoder)
+end
+
 @on_event function(sm::DriverLifecycle, ::Running, ::Tick, state::DriverState)
     return driver_lifecycle_tick!(state)
 end
